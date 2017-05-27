15ff93f40a3aba20e5b02c4cbe2b9dcd573f51e0
xandmore <xandmore3@XANDMORE-PC>
2017-05-27 23:36:57 +0300

    Project settings Edited

---
+++ Binarization/Binarizators/BinaryzatorType1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binarization.Binarizators
+{
+    // thresholding называется пороговая бинаризация
+    public class BinaryzatorType1 : BinaryzatorBase
+    {
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
